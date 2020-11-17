91eaa9345eb0cc1e4c9c69c13ab42e9dc4604833
VDLPYL <63089508+VDLPYL@users.noreply.github.com>
2020-11-17 01:50:03 -0500

    Create scripts.js

---
+++ 4VIRGENCITADELAPURAYLIMPIA/PENSAMIENTO/scripts.js
@@ -0,0 +1,15 @@
+// Esto es un comentario de una linea
+/* Este es un comentario de varias lineas */
+
+/**
+* Este también es un comentario de bloque
+* 
+*/
+
+
+
+/** TIPOS DE DATOS
+* number { console.log(typeof 50)}
+*
+*/
+console.log(typeof 50)
