3faaaf9cf0adb1fd44852f7a2d8e9be3649ee63b
VDLPYL <63089508+VDLPYL@users.noreply.github.com>
2020-11-26 12:59:28 -0500

    Delete index.js

--- 4VIRGENCITADELAPURAYLIMPIA/PENSAMIENTO/js/index.js
+++
@@ -1,8 +0,0 @@
-const title = document.getElementById(`title`)
-const title2 = document.querySelector(`#title`)
-const listItems = document.querySelectorAll(`ul li`)
-title.style.background = `yellow`;
-
-console.log(title)
-console.log(title2)
-console.log(listItems[2])
