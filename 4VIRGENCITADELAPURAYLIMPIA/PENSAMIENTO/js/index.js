56dfcf722d4fce1555d5dceae1fccd9717af6727
VDLPYL <63089508+VDLPYL@users.noreply.github.com>
2020-11-26 12:58:26 -0500

    Create index.js

---
+++ 4VIRGENCITADELAPURAYLIMPIA/PENSAMIENTO/js/index.js
@@ -0,0 +1,8 @@
+const title = document.getElementById(`title`)
+const title2 = document.querySelector(`#title`)
+const listItems = document.querySelectorAll(`ul li`)
+title.style.background = `yellow`;
+
+console.log(title)
+console.log(title2)
+console.log(listItems[2])
